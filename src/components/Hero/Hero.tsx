5a364824f853106169317ae14fc92d1da1dfb7e2
Scynes <dustin.greyfield@gmail.com>
2023-09-10 13:34:37 -0400

    wip(Hero): added Hero skeleton component

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import { Box } from '@chakra-ui/react';
+
+const Hero = () => {
+    return (
+        <Box height={ 'calc(100vh)' } bgSize={ 'cover' } bgPosition={ 'center' } bgRepeat={ 'no-repeat' }>
+            <p>Hello</p>
+        </Box>
+    );
+}
+
+export default Hero;
