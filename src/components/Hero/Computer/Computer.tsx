8602a1b3fcb8ef73c116e0b5601ba625b619aeb1
Scynes <dustin.greyfield@gmail.com>
2023-09-11 12:27:13 -0400

    util: created a custom hook (useWindowSizeMobile) for monitoring specific mobile sizes and added it to Computer component to decouple the usEffect logic

--- src/components/Hero/Computer/Computer.tsx
+++ src/components/Hero/Computer/Computer.tsx
@@ -1,44 +1,25 @@
 import { Environment, OrbitControls, Preload, useGLTF } from '@react-three/drei';
-import { Suspense, useEffect, useState } from 'react';
+import { Suspense } from 'react';
 
 import { Canvas, useThree } from '@react-three/fiber';
+import useWindowSizeMobile from '@hooks/useWindowSizeMobile';
 
 const Scene = () => {
-
-    const [ position, setPosition ] = useState([ 0, window.innerWidth <= 380 ? -9.5 : -4.5, -0.8 ]);
 
     const model = useGLTF('./models/desktop_pc/scene.gltf');
 
     const { viewport } = useThree();
 
-    // Define minimum and maximum scale values
-    const minScale = 0.45;
-    const maxScale = 1.05;
-
-    // Calculate the initial scale based on the viewport width
-    const initialScale = viewport.width / 20;
+    const { isMobile } = useWindowSizeMobile(380);
 
     // Calculate the scale factor to fit within the specified range
-    const scaleFactor = Math.min(Math.max(initialScale, minScale), maxScale);
-
-    // Update the position when the screen width changes
-    const handleResize = () => {
-        setPosition([ 0, window.innerWidth <= 380 ? -9.5 : -4.5, -0.8]);
-    };
-
-    useEffect(() => {
-        window.addEventListener('resize', handleResize);
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        }
-    }, []);
+    const scaleFactor = Math.min(Math.max((viewport.width / 20), 0.45), 1.05);
 
     return (
         <mesh scale={[scaleFactor, scaleFactor, scaleFactor]}>
             <pointLight intensity={ 1 } />
             <Environment preset={ 'forest' } />
-            <primitive object={ model.scene } position={ position } rotation={ [ 0, -0.8, -0.05 ] }/>
+            <primitive object={ model.scene } position={ [ 0, isMobile ? -9.5 : -4.5, -0.8 ] } rotation={ [ 0, -0.8, -0.05 ] }/>
         </mesh>
     );
 }
