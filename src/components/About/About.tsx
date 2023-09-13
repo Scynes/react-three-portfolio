77a5c0a92edc09da9e5499c586cb5afa7a2a5611
Scynes <dustin.greyfield@gmail.com>
2023-09-12 21:46:35 -0400

    fix/wip(About): refactored how the About section renders content using flex container. added aditonal motion animation

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import Tilt from 'react-parallax-tilt';
 
 import withSectionWrapper from '@hoc/withSectionWrapper';
+import { fadeIn, textDrop } from '@utils/motion';
 
 interface Service {
     icon: string,
@@ -31,29 +32,29 @@ const services: Service[] = [
 const About = () => {
 
     return (
-        <>
-            <Box as={ motion.div }>
-                <Text>INTRODUCTION</Text>
-                <Heading size={ { base: '2xl', md: '3xl' } }>Overview.</Heading>
+        <Flex wrap={ 'wrap' } gap={ '10' }>
+            <Box width={ 'full' }>
+                <Box as={ motion.div }>
+                    <Text>INTRODUCTION</Text>
+                    <Heading size={ { base: '2xl', md: '3xl' } }>Overview.</Heading>
+                </Box>
+                <Text as={ motion.p } variants={ textDrop(0.5) } maxWidth={ '3xl' }>
+                    Hey there! I'm absolutely stoked about coding and I'm on the lookout for a place where I can dive into some serious development action. TypeScript and JavaScript are like music to my ears, though I've also got a solid grasp of Python and Java. When it comes to frameworks, I'm your go-to person for React, SvelteKit, Django, and Node. Let's team up and start turning those brilliant ideas into functional, user-friendly solutions!
+                </Text>
             </Box>
-            <Text as={ motion.p }>
-                Hey there! I'm absolutely stoked about coding and I'm on the lookout for a place where I can dive into some serious development action. TypeScript and JavaScript are like music to my ears, though I've also got a solid grasp of Python and Java. When it comes to frameworks, I'm your go-to person for React, SvelteKit, Django, and Node. Let's team up and start turning those brilliant ideas into functional, user-friendly solutions!
-            </Text>
-            <Flex wrap={ 'wrap' } gap={ '10' } justifyContent={ 'center' }>
             { services.map((service, index) => (
-                <Tilt key={ index } scale={1.1} tiltReverse={ true }>
-                    <Box padding={ '2px' } rounded={ '2xl' } bgGradient={ 'linear(to-b, #804dee, #0073FF)' }>
-                        <Card width={ '14rem' } height={ '17rem' } boxShadow={ 'dark-lg' } rounded={ '2xl' }>
+                <Box as={ Tilt } width={ 'full' } maxWidth={ { base: 'full', sm: '240px', md: '240px' } } key={ index } scale={1.1} tiltReverse={ true }>
+                    <Box as={ motion.div } variants={ fadeIn('right', 'spring', index * 0.5, 0.75 ) } padding={ '2px' } rounded={ '2xl' } bgGradient={ 'linear(to-b, #804dee, #0073FF)' }>
+                        <Card height={ '17rem' } boxShadow={ 'dark-lg' } rounded={ '2xl' }>
                             <Center height={ 'full' } flexDirection={ 'column' } gap={ '2rem' }>
                                 <Image src={ `/icons/${service.icon}-icon.png` } />
-                                <Text fontWeight={ 'bold' } fontSize={ '2xl' } maxW={ '8rem' } textAlign={ 'center' }>{ service.title }</Text>
+                                <Text fontWeight={ 'bold' } fontSize={ '3xl' } maxW={ '9rem' } textAlign={ 'center' }>{ service.title }</Text>
                             </Center>
                         </Card>
                     </Box>
-                </Tilt>
+                </Box>
             ))}
-            </Flex>
-        </>
+        </Flex>
     );
 }
 
--- src/hoc/withSectionWrapper.tsx
+++ src/hoc/withSectionWrapper.tsx
@@ -1,4 +1,5 @@
 import { Box } from '@chakra-ui/react';
+import { stagger } from '@utils/motion';
 import { motion } from 'framer-motion';
 
 const withSectionWrapper = ( Component: React.FC, name: string ) => (
@@ -6,7 +7,7 @@ const withSectionWrapper = ( Component: React.FC, name: string ) => (
     function HOC () {
 
         return (
-            <Box as={ motion.section }>
+            <Box as={ motion.section } variants={ stagger() } initial={ 'hidden' } whileInView={ 'show' } viewport={ { once: true, amount: 0.25 } } marginX={ 'auto' } maxWidth={ '6xl' } paddingX={ '32px' }>
                 <Component />
             </Box>
         )
