98000813019c60b6f61f3e65650f01883ce1cf25
Scynes <dustin.greyfield@gmail.com>
2023-09-13 14:26:03 -0400

    tweak: adjusted hero-bg SVG colors and fixed code formatting in the About/Card component

--- src/components/About/Card/Card.tsx
+++ src/components/About/Card/Card.tsx
@@ -14,15 +14,15 @@ interface Properties {
 const Card  = ( { slot, icon, title }: Properties ) => {
     return (
         <Box as={ Tilt } width={ 'full' } maxWidth={ { base: 'full', sm: '240px', md: '240px' } } key={ slot } tiltReverse={ true }>
-        <Box as={ motion.div } variants={ fadeIn('right', 'spring', slot * 0.5, 0.75 ) } padding={ '2px' } rounded={ '2xl' } bgGradient={ 'linear(to-b, #804dee, #0073FF)' }>
-            <ChakraCard height={ '17rem' } boxShadow={ 'dark-lg' } rounded={ '2xl' }>
-                <Center height={ 'full' } flexDirection={ 'column' } gap={ '2rem' }>
-                    <Image src={ `/icons/${icon}-icon.png` } />
-                    <Text fontWeight={ 'bold' } fontSize={ '3xl' } maxW={ '9rem' } textAlign={ 'center' }>{ title }</Text>
-                </Center>
-            </ChakraCard>
+            <Box as={ motion.div } variants={ fadeIn('right', 'spring', slot * 0.5, 0.75 ) } padding={ '2px' } rounded={ '2xl' } bgGradient={ 'linear(to-b, #804dee, #0073FF)' }>
+                <ChakraCard height={ '17rem' } boxShadow={ 'dark-lg' } rounded={ '2xl' }>
+                    <Center height={ 'full' } flexDirection={ 'column' } gap={ '2rem' }>
+                        <Image src={ `/icons/${icon}-icon.png` } />
+                        <Text fontWeight={ 'bold' } fontSize={ '3xl' } maxW={ '9rem' } textAlign={ 'center' }>{ title }</Text>
+                    </Center>
+                </ChakraCard>
+            </Box>
         </Box>
-    </Box>
     );
 }
 
