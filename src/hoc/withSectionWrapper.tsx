512adb50b980ae938704d249bcda5f177464af93
Scynes <dustin.greyfield@gmail.com>
2023-10-03 20:58:32 -0400

    fix: added name as id to withSectionWrapper Box

--- src/hoc/withSectionWrapper.tsx
+++ src/hoc/withSectionWrapper.tsx
@@ -7,7 +7,7 @@ const withSectionWrapper = ( Component: React.FC, name: string ) => (
     function HOC () {
 
         return (
-            <Box as={ motion.section } variants={ stagger() } initial={ 'hidden' } whileInView={ 'show' } viewport={ { once: true, amount: 0.25 } } marginX={ 'auto' } maxWidth={ '6xl' } padding={ '32px 32px 64px 32px' } overflow={ 'hidden' }>
+            <Box id={ name } as={ motion.section } variants={ stagger() } initial={ 'hidden' } whileInView={ 'show' } viewport={ { once: true, amount: 0.25 } } marginX={ 'auto' } maxWidth={ '6xl' } padding={ '32px 32px 64px 32px' } overflow={ 'hidden' }>
                 <Component />
             </Box>
         )
