fa301a337512e45ea6e9f8ad4776d2155c476a23
Scynes <dustin.greyfield@gmail.com>
2023-10-16 12:12:38 -0400

    fix: added use agent detection for android devices as a temporary fix for 3d rendering of tech causing a webGL context error. technology instead renders as tags on those devices

--- src/components/Technology/Technology.tsx
+++ src/components/Technology/Technology.tsx
@@ -1,7 +1,8 @@
 import SectionHeading from '@components/SectionHeading';
 import withSectionWrapper from '@hoc/withSectionWrapper';
 import Ball from './Ball';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Image, Tag } from '@chakra-ui/react';
+import { isAndroid } from 'react-device-detect';
 
 const TECH = [
     'angular',
@@ -28,13 +29,23 @@ const Technology = () => {
         <>
             <SectionHeading label={ 'WHAT I USE' } heading={ 'Technology.' } />
             <Flex justifyContent={ 'center' } marginTop={ '3rem' }>
-                <Flex gap={ '2rem' } wrap={ 'wrap' } justifyContent={ 'center' } maxWidth={ '3xl' }>          
-                    { TECH.map((tech, index) => (
-                        <Box key={ index } boxSize={20}>
-                            <Ball icon={ tech } />
-                        </Box>
-                    )) }
-                </Flex>
+                { !isAndroid ?
+                    <Flex gap={ '2rem' } wrap={ 'wrap' } justifyContent={ 'center' } maxWidth={ '3xl' }>          
+                        { TECH.map((tech, index) => (
+                            <Box key={ index } boxSize={20}>
+                                <Ball icon={ tech } />
+                            </Box>
+                        )) }
+                    </Flex>
+                : 
+                    <Flex width={ 'full' } wrap={ 'wrap' } gap={ '1rem' } justifyContent={'center'}>
+                        { TECH.map((tech, index) => (
+                            <Tag padding={ '0.5rem' } key={ index } size={ 'lg' } variant={ 'solid' } colorScheme={ 'purple' }>
+                                <Image boxSize={ '2rem' } src={ `/icons/technology/${ tech }.png` } />
+                            </Tag>
+                        )) }
+                    </Flex>
+                }
             </Flex>
         </>
     );
